ce61399ff34f41afff45cf4988969f3695652862
uynv <uynguyenvan1608@gmail.com>
2026-03-11 13:18:07 +0700

    test blink led tren lab

--- main.c
+++ main.c
@@ -1,16 +1,62 @@
 #include "stm32f10x.h"
 
-void delay(uint32_t time) {
-    while(time--);
+void Delay1Ms(void);
+void Delay_Ms(uint32_t u32DelayInMs);
+
+void Delay1Ms(void)
+{
+	
+	TIM_SetCounter(TIM2, 0);
+	while (TIM_GetCounter(TIM2) < 1000) {
+	}
 }
 
-int main(void) {
-    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-    GPIOC->CRH &= ~(0xF << 20);
-    GPIOC->CRH |= (0x3 << 20);
-    
-    while(1) {
-        GPIOC->ODR ^= (1 << 13);
-        delay(500000);
-    }
+void Delay_Ms(uint32_t u32DelayInMs)
+{
+	
+	while (u32DelayInMs) {
+		Delay1Ms();
+		--u32DelayInMs;
+	}
+}
+
+int main(void)
+{
+	GPIO_InitTypeDef gpioInit;
+	TIM_TimeBaseInitTypeDef timerInit;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	
+	gpioInit.GPIO_Mode = GPIO_Mode_Out_PP;
+	gpioInit.GPIO_Pin = GPIO_Pin_13;
+	gpioInit.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOC, &gpioInit);
+	
+	gpioInit.GPIO_Mode = GPIO_Mode_Out_PP;
+	gpioInit.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	gpioInit.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOB, &gpioInit);
+	
+	timerInit.TIM_CounterMode = TIM_CounterMode_Up;
+	timerInit.TIM_Period = 0xFFFF;
+	timerInit.TIM_Prescaler = 72 - 1;
+	
+	TIM_TimeBaseInit(TIM2, &timerInit);
+	
+	TIM_Cmd(TIM2, ENABLE);
+	
+	
+	while (1) {
+		GPIO_SetBits(GPIOC, GPIO_Pin_13);
+		//GPIO_SetBits(GPIOB, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+		Delay_Ms(100);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+		//GPIO_ResetBits(GPIOB, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+		Delay_Ms(100);
+	}
+	
 }
